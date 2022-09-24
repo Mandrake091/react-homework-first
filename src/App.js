5fcdae8a9974a4ca88a423ecd757a604d3f2c170
Mandrake091 <wasdgamer91@gmail.com>
2022-09-24 12:24:25 +0200

    scaffholding - install bootstrap

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, {useState} from 'react';
+
+function App() {
+const adminUser = {
+  email:'admin@admin.com',
+  password: 'admin'
+}
+
+
+
+
+
+
+
+
+
+  return (
+    <div className="App">
+      hello
+     
+    </div>
+  );
+}
+
+export default App;
